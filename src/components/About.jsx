baf9064438452222c9b84466e1126f8798dcc4a9
rhani <rhani@HENRYS-LAPTOP>
2023-07-05 23:13:15 +0530

    bottom 😔

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,14 +4,46 @@ const About = () => {
   return (
     <div className="grid grid-cols-4 gap4 text-white justify-evenly  m-5 place-content-evenly">
       <div className="">
-        <p 
-        style={{
+        <p
+          style={{
             fontWeight: 600,
-        }}>Hey</p>
+          }}
+        >
+          Hey:
+        </p>
+        <p className="mt-2.5">My name is Henry</p>
       </div>
-      <div className="">About</div>
-      <div className="">About</div>
-      <div className="">About</div>
+      <div>
+        <p
+          style={{
+            fontWeight: 600,
+          }}
+        >
+          Projects:
+        </p>
+        <ol className=" list-inside list-decimal mt-2.5">
+          <li>Tick Tock..</li>
+          <li>Stopple</li>
+          <li>Tick Tock..</li>
+          <li>Tick Tock..</li>
+        </ol>
+      </div>
+      <div>
+        <p
+          style={{
+            fontWeight: 600,
+          }}
+        >
+        <br></br>
+        </p>
+        <ol className=" list-inside list-decimal mt-2.5">
+          <li>Tick Tock..</li>
+          <li>Stopple</li>
+          <li>Tick Tock..</li>
+          <li>Tick Tock..</li>
+        </ol>
+      </div>
+      <div>About</div>
     </div>
   );
 };
