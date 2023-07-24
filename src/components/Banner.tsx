cb75be0c5a6fb75f67de2c9b7e308f9c50ce8d90
rhani <rhani@HENRYS-LAPTOP>
2023-07-24 17:55:20 +0100

    ??

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,13 +3,13 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="">
-      <div className="fixed flex z-50 rotate-3 top-48 shadow-lg select-none">
+      {/* <div className="fixed flex z-[-1] rotate-3 top-48 shadow-lg select-none">
         <div className="whitespace-nowrap ml-1 mr-1">
           <div className="flex gap-2 animate-rightToLeft bg-[#30c104] p-2 border-4 border-black border-l-0 shadow-lg text-black">
             <p className=""
               style={{
                 fontWeight: 600,
-                fontSize: 30,
+                fontSize: 28,
               }}
             >
               🚧 THIS SITE IS CURRENTLY UNDER DEVELOPMENT 🚧 THIS SITE IS
@@ -19,7 +19,7 @@ const Banner = () => {
             <p
               style={{
                 fontWeight: 600,
-                fontSize: 30,
+                fontSize: 28,
               }}
             >
               🚧 THIS SITE IS CURRENTLY UNDER DEVELOPMENT 🚧 THIS SITE IS
@@ -28,15 +28,15 @@ const Banner = () => {
             </p>
           </div>
         </div>
-      </div>
+      </div> */}
 
-      <div className="fixed flex z-[-50] -rotate-12 top-24 select-none -translate-x-20">
+      <div className="fixed flex z-[50] -rotate-12 top-18 select-none -translate-x-20">
         <div className="whitespace-nowrap ml-1 mr-1">
-          <div className="flex gap-4 animate-leftToRight bg-[#2ca707] p-2 border-4 border-black border-l-0 shadow-lg">
+          <div className="flex gap-4 animate-leftToRight bg-[#33d207] p-2 border-4 border-black border-l-0 shadow-lg">
             <p
               style={{
                 fontWeight: 600,
-                fontSize: 28,
+                fontSize: 30,
               }}
             >
               🚧 THIS SITE IS CURRENTLY UNDER DEVELOPMENT 🚧 THIS SITE IS
@@ -46,7 +46,7 @@ const Banner = () => {
             <p
               style={{
                 fontWeight: 600,
-                fontSize: 28,
+                fontSize: 30,
               }}
             >
               🚧 THIS SITE IS CURRENTLY UNDER DEVELOPMENT 🚧 THIS SITE IS
@@ -57,13 +57,13 @@ const Banner = () => {
         </div>
       </div>
 
-      <div className="fixed flex z-50 rotate-6 bottom-5 shadow-lg select-none -translate-x-10">
+      <div className="fixed flex z-[-1] rotate-6 bottom-40 shadow-lg select-none -translate-x-10">
         <div className="whitespace-nowrap ml-1 mr-1">
           <div className="flex gap-2 animate-rightToLeft bg-[#04c12a] p-2 border-4 border-black border-l-0 shadow-lg">
             <p
               style={{
                 fontWeight: 600,
-                fontSize: 30,
+                fontSize: 28,
               }}
             >
               🚧 THIS SITE IS CURRENTLY UNDER DEVELOPMENT 🚧 THIS SITE IS
@@ -73,7 +73,7 @@ const Banner = () => {
             <p
               style={{
                 fontWeight: 600,
-                fontSize: 30,
+                fontSize: 28,
               }}
             >
               🚧 THIS SITE IS CURRENTLY UNDER DEVELOPMENT 🚧 THIS SITE IS
